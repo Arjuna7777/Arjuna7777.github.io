c79605f3cc9b3a229534253b425b3e71d7e4e18e
Arjuna7777 <126076049+Arjuna7777@users.noreply.github.com>
2023-05-03 21:23:33 +0300

    Update code_dz7.js

--- week7/code_dz7.js
+++ week7/code_dz7.js
@@ -14,44 +14,13 @@ scene.add(cylinder);
 cylinder.position.z = -25;
 cylinder.position.x = 5;
 
+const spheregeometry = new THREE.SphereGeometry(1, 16, 16);
+const spherematerial = new THREE.MeshBasicMaterial({ color: 0x00FF00 });
+const sphere = new THREE.Mesh(spheregeometry, spherematerial);
+scene.add(sphere);
+sphere.position.x = -3;
+
 const boxgeometry = new THREE.BoxGeometry(2, 2, 2);
-const boxmaterial = new THREE.MeshBasicMaterial({ color: 0x00FF00 });
+const boxmaterial = new THREE.MeshBasicMaterial({ color: 0x0000FF });
 const cube = new THREE.Mesh(boxgeometry, boxmaterial);
-scene.add(cube);
-
-var lightOne=new THREE.AmbientLight(0x0000FF, 0.5);
-scene.add(lightOne);
-
-var lightTwo=new THREE.PointLight(0x0000FF, 0.5);
-scene.add(lightTwo);
-
-lightTwo.position.set(25, 0, -25)
-
-camera.position.z = 7;
-camera.position.x = 2;
-
-renderer.setClearColor(0x0000FF);
-renderer.clear();
-
-let angle = 0, radius = 47;
-
-function animate() {
-  requestAnimationFrame(animate);
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  cylinder.rotation.x += 0.02;
-  cylinder.rotation.y += 0.02;
-
-  lightTwo.position.x = radius * Math.cos(angle) + 5;
-  lightTwo.position.y = radius * Math.sin(angle);
-
-  angle += Math.PI / 180;
-
-  camera.lookAt(scene.position);
-
-  renderer.render(scene, camera);
-}
-
-animate();
+scene
