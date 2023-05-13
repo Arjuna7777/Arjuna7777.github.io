37b6f3b93c202f8d51eed7b9a7a0ddc2d421e4fd
Arjuna7777 <126076049+Arjuna7777@users.noreply.github.com>
2023-05-13 11:21:05 +0300

    Update code_dz9.js

--- week9/code_dz9.js
+++ week9/code_dz9.js
@@ -24,7 +24,7 @@ window.addEventListener("DOMContentLoaded", () => {
     // Load a glTF resource
     loader.load(
       // resource URL
-      'train.glb',
+      'IronMan.obj',
       // called when the resource is loaded
       function ( gltf ) {
          //gltf.scene.scale.set(0.1, 0.1, 0.1);
