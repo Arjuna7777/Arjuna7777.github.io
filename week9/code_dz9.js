e874ff713824d6d01f35a9259271a6e0d84343bb
Arjuna7777 <126076049+Arjuna7777@users.noreply.github.com>
2023-05-13 14:07:57 +0300

    Update code_dz9.js

--- week9/code_dz9.js
+++ week9/code_dz9.js
@@ -24,7 +24,7 @@ window.addEventListener("DOMContentLoaded", () => {
     // Load a glTF resource
     loader.load(
       // resource URL
-      'IronMan.obj',
+      'tyrannosaurus_rex.glb',
       // called when the resource is loaded
       function ( gltf ) {
          //gltf.scene.scale.set(0.1, 0.1, 0.1);
